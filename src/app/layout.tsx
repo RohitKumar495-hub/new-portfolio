6435df8a92df084dba7a660d9066fdd59c4764d2
Rohit Kumar <kumarguptarohit2005@gmail.com>
2026-04-09 23:49:32 +0530

    error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,17 @@
-'use client'
+import Analytics from "@/components/Analytics";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Theme from "@/components/Theme";
 import Script from "next/script";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-import { pageview } from "@/lib/gtag";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // Track the current path
-  const pathname = usePathname();
-
-  // Fire GA page_view on every route change
-  useEffect(() => {
-    if (typeof window !== "undefined" && (window as any).gtag) {
-      pageview(pathname);
-    }
-  }, [pathname]);
-
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics 4 */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-D7CH7KET6G"
@@ -43,6 +29,8 @@ export default function RootLayout({
       </head>
 
       <body className="font-sans bg-black md:p-6">
+        <Analytics /> {/* ✅ moved here */}
+
         <div className="grid lg:grid-cols-[95%_5%]">
           <main className="relative">
             <div className="fixed top-1 lg:top-4 lg:right-10 right-2 z-10">
